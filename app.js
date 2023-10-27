20992cdcc9d0a255d4b25c5a79cff6937d50ba83
neginkou <negin.kou@gmail.com>
2023-10-26 19:43:35 -0700

    corrections

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Welcome user
+let userName; // Declare userName in a higher scope
+
 function welcome() {
-const userName = prompt("Welcome! Please enter your name:");
-const welcomeMessage = `Welcome to Negin's Life Journey, ${userName}! Let's get to know each other better.`;
-alert(welcomeMessage);
+  userName = prompt("Welcome! Please enter your name:");
+  const welcomeMessage = `Welcome to Negin's Life Journey, ${userName}! Let's get to know each other better.`;
+  alert(welcomeMessage);
 }
 welcome();
+
 // questions and answers
 function quiz() {
   let questions = [
@@ -15,49 +18,44 @@ function quiz() {
     "Am I transitioning to a career as a software developer? (Yes/No)",
   ];
 
-let answers = ["Yes", "Yes", "No", "Yes", "Yes"];
+  let answers = ["Yes", "Yes", "No", "Yes", "Yes"];
+  let correctAnswers = 0; // Initialize correctAnswers
 
-function checkAnswer(userInput, correctAnswer) {
-  return userInput.toLowerCase() === correctAnswer.toLowerCase();
-}
-
-for (let i = 0; i < questions.length; i++) {
-  const userResponse = prompt(questions[i]);
-  if (userResponse !== null) {
-    if (checkAnswer(userResponse, answers[i])) {
-      alert("Correct! You got it right.");
-    } else {
-      alert("Sorry, that's not correct");
-    }
-
+  function checkAnswer(userInput, correctAnswer) {
+    return userInput.toLowerCase() === correctAnswer.toLowerCase();
   }
-  alert(`you got ${correctAnswers} out of ${questions.length}question correct`);
-}
+
+  for (let i = 0; i < questions.length; i++) {
+    const userResponse = prompt(questions[i]);
+    if (userResponse !== null) {
+      if (checkAnswer(userResponse, answers[i])) {
+        alert("Correct! You got it right.");
+        correctAnswers++; // Increment correctAnswers
+      } else {
+        alert("Sorry, that's not correct");
+      }
+    }
+  }
+  alert(`You got ${correctAnswers} out of ${questions.length} questions correct`);
 }
 quiz();
 
-// function checkAnswer(userInput, correctAnswer) {
-//   return userInput.toLowerCase() === correctAnswer.toLowerCase();
-
-// if (userResponse !== null) {
-//   if (checkAnswer(userResponse, answers[i])) {
-//     alert("Correct! You got it right.");
 function favoritfruit() {
   const correctAnswers = ["apple", "pomegranate", "sour cherry"];
   let attempts2 = 6;
   let guessedCorrectly2 = false;
 
-while (attempts2 > 0) {
-  const userAnswer = prompt("What is my favorite fruit, Enter your guess:");
-  if (correctAnswers.includes(userAnswer.toLowerCase())) {
-    alert("Correct! That's one of the answers.");
-    guessedCorrectly2 = true;
-    break;
-  } else {
-    alert("Incorrect. Try again.");
+  while (attempts2 > 0) {
+    const userAnswer = prompt("What is my favorite fruit, Enter your guess:");
+    if (correctAnswers.includes(userAnswer.toLowerCase())) {
+      alert("Correct! That's one of the answers.");
+      guessedCorrectly2 = true;
+      break;
+    } else {
+      alert("Incorrect. Try again.");
+    }
+    attempts2--;
   }
-  attempts2--;
-}
 
   if (!guessedCorrectly2) {
     alert("You've run out of attempts. The correct answers are: " + correctAnswers.join(", "));
@@ -83,17 +81,19 @@ function guessage() {
       attempts--;
     } else {
       isCorrect = true;
-
+      break; // Break the loop when the correct answer is guessed
     }
   }
+
   if (isCorrect) {
     alert(`Congratulations! You guessed my age correctly, which is ${myAge}.`);
   }
 }
-function finalMessage(){
 guessage();
-// Final message
-const finalMessage = `Thank you for learning more about me, ${userName}. I hope you enjoyed the game!`;
-alert(finalMessage);
+
+function finalMessage() {
+  // Final message
+  const finalMessage = `Thank you for learning more about me, ${userName}. I hope you enjoyed the game!`;
+  alert(finalMessage);
 }
 finalMessage();
